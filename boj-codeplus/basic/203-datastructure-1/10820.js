2857c4477a740251354598b66437d9f31525961b
chotaegyu <chotg5592@gmail.com>
2021-08-19 21:43:40 +0900

    21.08.19

---
+++ boj-codeplus/basic/203-datastructure-1/10820.js
@@ -0,0 +1,7 @@
+let input = `This is String
+SPACE    1    SPACE
+ S a M p L e I n P u T     
+0L1A2S3T4L5I6N7E8`
+  .trim()
+  .split('\n');
+// input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
