cab0ff86c585a7fd9d2901cb3deaa84fb3beac5f
chotaegyu <chotg5592@gmail.com>
2022-08-13 23:50:28 +0900

    코딩연습 24: Linear Search Exercise

---
+++ udemy/10.검색알고리즘/linear_search_exercise.js
@@ -0,0 +1,13 @@
+function linearSearch(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) return i;
+  }
+  return -1;
+}
+
+linearSearch([10, 15, 20, 25, 30], 15); // 1
+linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4); // 5
+linearSearch([100], 100); // 0
+linearSearch([1, 2, 3, 4, 5], 6); // -1
+linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10); // -1
+linearSearch([100], 200); // -1
